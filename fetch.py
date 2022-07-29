58ae08b9948d7d3b52b58505be8ad0e7fd794e38
willriley <wriley.email@gmail.com>
2022-07-29 02:15:18 -0400

    complete first pass. formatted. debug later

--- fetch.py
+++ fetch.py
@@ -3,13 +3,21 @@ import argparse
 import pprint
 from datetime import datetime, timedelta
 from dataclasses import dataclass
+import csv
+
+# run with number of days specified and output file
+# returns a csv where columns are fields we care about
+# and rows are games that meet our requirements
 
 START_INNING = 6
 RUN_THRESHOLD = 8
+CSV_HEADERS = ['date', 'home', 'away', 'pos?', 'pos team', 'pos name', 'pos runs',
+               'pos num pitches', 'score 6', 'score 7', 'score 8',
+               'score 9', 'final score', 'pitchers 6', 'pitchers 7',
+                'pitchers 8', 'pitchers 9', 'final pitchers']
 
-# run with number of days specified
-# returns a csv where columns are fields we care about
-# and rows are games that meet our requirements
+# 'score 6' => (home score after 6, away score after 6)
+# 'final pitchers' => (home total pitchers used, away total pitchers used)
 
 @dataclass
 class GameData:
@@ -22,8 +30,9 @@ class GameData:
     pos_team: str = ''  # 'Home' or 'Away' (or '' if no pos)
     pos_runs: int = 0
     pos_num_pitches: int = 0
+    innings = int = 9
     # map from inning to home score, starting at START_INNING until end of game
-    home_score_after: dict[int, int] = {} #
+    home_score_after: dict[int, int] = {}
     away_score_after: dict[int, int] = {}
     # map from inning to num pitchers, starting at START_INNING until end of game
     home_pitchers_after: dict[int, int] = {}
@@ -31,12 +40,32 @@ class GameData:
 
     def should_log(self) -> bool:
     """Whether we should log this game."""
-        # check if score difference is >= RUN_THRESHOLD
-        is_blowout = False
-        for i in range(START_INNING, START_INNING + len(self.home_score_after)):
-            is_blowout |= (abs(home_score_after[i] - away_score_after[i]) >= RUN_THRESHOLD)
+      # check if score difference is >= RUN_THRESHOLD
+      is_blowout = False
+       for i in range(START_INNING, START_INNING + len(self.home_score_after)):
+            is_blowout |= (
+                abs(home_score_after[i] - away_score_after[i]) >= RUN_THRESHOLD)
 
         return is_blowout or self.is_pos
+
+    def to_csv_row(self):
+    """CSV row representation of this GameData object."""
+      def check(val):
+           return val if val else 'null'
+
+    return [self.date, self.home_team, self.away_team, str(self.is_pos)[0],
+            check(self.pos_team), check(self.pos_name), self.pos_runs,
+            self.pos_num_pitches,
+            f"{self.home_score_after[6]-self.away_score_after[6]}",
+            f"{self.home_score_after[7]-self.away_score_after[7]}",
+            f"{self.home_score_after[8]-self.away_score_after[8]}",
+            f"{self.home_score_after[9]-self.away_score_after[9]}",
+            f"{self.home_score_after[self.innings]-self.away_score_after[self.innings]}",
+            f"{self.home_pitchers_after[6]-self.away_pitchers_after[6]}",
+            f"{self.home_pitchers_after[7]-self.away_pitchers_after[7]}",
+            f"{self.home_pitchers_after[8]-self.away_pitchers_after[8]}",
+            f"{self.home_pitchers_after[9]-self.away_pitchers_after[9]}",
+            f"{self.home_pitchers_after[self.innings]-self.away_pitchers_after[self.innings]}"]
 
 
 def parse_line(linescore, innings):
@@ -108,13 +137,16 @@ def get_pos(boxscore):
     home_pos = find_pos(boxscore['homePitchers'])
     return ('Home', home_pos)
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--days', type=int, default=30)
+parser.add_argument('-f', '--file', type=str, default='games.csv')
+parsed_args = parser.parse_args()
 
 now = datetime.now()
 today = now.strftime('%m/%d/%Y')
 
-start = now - timedelta(days=parser.parse_args().days)
+start = now - timedelta(days=parsed_args.days)
 start_date = start.strftime('%m/%d/%Y')
 
 games = statsapi.schedule(start_date=start_date, end_date=today)
@@ -123,10 +155,13 @@ filtered_games = []
 for game in games:
     if game['status'] != 'Final':
         gid = game['game_id']
-        game_data = GameData(game['game_date'], game['home_name'], game['away_name'])
+        game_data = GameData(
+            game['game_date'], game['home_name'], game['away_name'])
 
         linescore = statsapi.linescore(gid)
-        game_data.home_score_after, game_data.away_score_after = parse_line(linescore, game['current_inning'])
+        game_data.inning = game['current_inning']
+        game_data.home_score_after, game_data.away_score_after = parse_line(
+            linescore, game_data.inning)
 
         boxscore = statsapi.boxscore_data(gid)
         pos_team, pos = get_pos(boxscore)
@@ -139,26 +174,16 @@ for game in games:
             game_data.pos_runs = pos['r']
             game_data.pos_num_pitches = pos['p']
 
-        game_data.home_pitchers_after, game_data.away_pitchers_after = parse_pitchers(boxscore)
+        game_data.home_pitchers_after, game_data.away_pitchers_after = parse_pitchers(
+            boxscore)
 
         if game_data.should_log():
             filtered_games.append(game_data)
-            # log to csv
 
 # TODO: exceptions, csv logging, debugging
+with open(parsed_args.file, 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
 
-# pprint.pp(games)
-
-# pprint.pp(statsapi.boxscore_data(games[0]['game_id']))
-# pprint.pp(statsapi.boxscore_data(661560))
-# for player in statsapi.lookup_player(657272):
-#     pprint.pp(player)
-#
-# pprint.pp(statsapi.linescore(661560))
-
-# TODO:
-
-# game 662357 10 innings
-# extra_inning_scoreline = statsapi.linescore(662357)
-# print(extra_inning_scoreline)
-# parse_line(extra_inning_scoreline, 10)
+    game_rows = [game.to_csv_row() for game in filtered_games]
+    writer.writerows(game_rows)
